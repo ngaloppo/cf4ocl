083abc9c87aff99516f0e3620ea17337c8528dfa
Nuno Fachada <nfachada@laseeb.org>
2014-03-05 20:42:05 +0000

    Add v1.3.0 API docs

---
+++ docs/v1.3.0/search/variables_65.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['end',['end',['../structcl_event.html#ade7103d21c95868cfe28ce77897ec8cb',1,'clEvent']]],
+  ['event_5finstants',['event_instants',['../structprofcl__profile.html#ab9d7da460ea1f5caf70824cba8a41fd6',1,'profcl_profile']]],
+  ['eventname',['eventName',['../structprofcl__evinst.html#a588256e9f596ec4a4d1396a554a68d3f',1,'profcl_evinst::eventName()'],['../structprofcl__ev__aggregate.html#a588256e9f596ec4a4d1396a554a68d3f',1,'profcl_ev_aggregate::eventName()']]],
+  ['evname_5fdelim',['evname_delim',['../structprofcl__export__options.html#a954c00e12170d5bb434613b21431d4dc',1,'profcl_export_options']]]
+];
